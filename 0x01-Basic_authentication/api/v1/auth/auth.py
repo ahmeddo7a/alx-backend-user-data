cde6a5015056c91011a9f24e97550119b6b7e43f
ahmeddo7a <ahmedholdo@gmail.com>
2024-01-18 04:05:03 +0200

    define routes dont need aithentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,13 +6,33 @@ from typing import List, TypeVar
 import fnmatch
 
 
+def compare_strings(str1, str2):
+    # Add a trailing backslash to str1 if it doesn't have one
+    if str1[-1] != '/':
+        str1 += '/'
+
+    # Compare the modified str1 with str2
+    return str1 == str2
+
+
 class Auth:
     """ authentication Class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method to check if auth is required.
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            compare_result = compare_strings(path, excluded_path)
+            if compare_result:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method to get authorization header.
